bc8866d1c270ffba79d18934790238519313112c
Trung <89704613+trungpsu1210@users.noreply.github.com>
2023-08-10 11:36:15 -0700

    Add files via upload

---
+++ predict.py
@@ -0,0 +1,122 @@
+import os
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from collections import OrderedDict
+from utils import AverageMeter, write_img, chw_to_hwc
+from data_loader import SingleLoader
+from models import *
+import cv2
+import pyiqa
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--model_test', default='testFELI_t', type=str, help='test model name')
+parser.add_argument('--model_train', default='trainFELI_t', type=str, help='train model name')
+parser.add_argument('--num_workers', default=16, type=int, help='number of workers')
+parser.add_argument('--data_dir', default='/root/', type=str, help='path to dataset')
+parser.add_argument('--save_dir', default='./saved_models/', type=str, help='path to models saving')
+parser.add_argument('--result_dir', default='./results/', type=str, help='path to results saving')
+parser.add_argument('--dataset', default='Dataset Blur', type=str, help='dataset name')
+parser.add_argument('--testset', default='realworld_test', type=str, help='testset name')
+parser.add_argument('--exp', default='lowlight_blur', type=str, help='experiment setting')
+args = parser.parse_args()
+
+def check_image_size(x, down_factor):
+    _, _, h, w = x.size()
+    mod_pad_h = (down_factor - h % down_factor) % down_factor
+    mod_pad_w = (down_factor - w % down_factor) % down_factor
+    x = F.pad(x, (0, mod_pad_w, 0, mod_pad_h), 'reflect')
+    return x
+
+def single(save_dir):
+	state_dict = torch.load(save_dir)['state_dict']
+	
+	moduleA_state_dict = {k: v for k, v in state_dict.items() if k.startswith('module.LRNet.main_LRNet')}
+		
+	new_state_dict = OrderedDict()
+
+	for k, v in moduleA_state_dict.items():
+		# name = k[7:]
+		name=k[24:]
+		new_state_dict[name] = v
+
+	return new_state_dict
+
+
+def predict(test_loader, network, result_dir, niqe, nrqm):
+
+	NIQE = AverageMeter()
+	NRQM = AverageMeter()
+
+	torch.cuda.empty_cache()
+
+	network.eval()
+
+	os.makedirs(os.path.join(result_dir, 'predict_imgs'), exist_ok=True)
+
+	for idx, batch in enumerate(test_loader):
+		input = batch['img'].cuda()
+		filename = batch['filename'][0]
+
+
+		with torch.no_grad():
+
+			torch.cuda.empty_cache()
+
+			H, W = input.shape[2:]
+			input = check_image_size(input, down_factor)
+			output, _, _ = network(input)
+					
+			output = output.clamp_(-1, 1)	
+	
+			# [-1, 1] to [0, 1]
+			output = output * 0.5 + 0.5
+
+			output = output[:,:,:H,:W]
+
+			niqe_metric = float(niqe(output))
+			nrqm_metric = float(nrqm(output))
+
+
+		NIQE.update(niqe_metric)
+		NRQM.update(nrqm_metric)
+
+
+		print('Test: [{0}]\t'
+			  'NIQE: {niqe.val:.04f} ({niqe.avg:.04f})\t'
+			  'NRQM: {nrqm.val:.04f} ({nrqm.avg:.04f})\t'
+			  'Name: {filename}'.format(idx, niqe=NIQE, nrqm=NRQM, filename=filename))
+
+		out_img = chw_to_hwc(output.detach().cpu().squeeze(0).numpy())
+		write_img(os.path.join(result_dir, 'predict_imgs', filename), out_img)
+
+
+if __name__ == '__main__':
+	network = eval(args.model_test)()
+	network.cuda()
+	saved_model_dir = os.path.join(args.save_dir, args.exp, args.model_train + '.pth')
+
+	down_factor = 8
+
+	if os.path.exists(saved_model_dir):
+		print('==> Start predicting, current model name: ' + args.model_test)
+		network.load_state_dict(single(saved_model_dir))
+	else:
+		print('==> No existing trained model!')
+		exit(0)
+
+	niqe = pyiqa.create_metric('niqe', device=torch.device("cuda"))
+	nrqm = pyiqa.create_metric('nrqm', device=torch.device("cuda"))
+
+	root_dir = os.path.join(args.data_dir, args.dataset, args.testset)
+	test_dataset = SingleLoader(root_dir)
+	test_loader = DataLoader(test_dataset,
+							 batch_size=1,
+							 num_workers=args.num_workers,
+							 pin_memory=True)
+
+
+	result_dir = os.path.join(args.result_dir, args.dataset, args.model_train)
+	predict(test_loader, network, result_dir, niqe, nrqm)
---
+++ train.py
@@ -0,0 +1,178 @@
+import os
+import argparse
+import json
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.cuda.amp import autocast, GradScaler
+from torch.utils.data import DataLoader
+from tensorboardX import SummaryWriter
+from tqdm import tqdm
+from utils import AverageMeter
+from data_loader import PairLoader
+from models import *
+from models.CR import *
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--model', default='trainFELI_t', type=str, help='train model name')
+parser.add_argument('--num_workers', default=16, type=int, help='number of workers')
+parser.add_argument('--no_autocast', action='store_false', default=True, help='disable autocast')
+parser.add_argument('--save_dir', default='./saved_models/', type=str, help='path to models saving')
+parser.add_argument('--data_dir', default='/root/', type=str, help='path to dataset')
+parser.add_argument('--log_dir', default='./logs/', type=str, help='path to logs')
+parser.add_argument('--dataset', default='Dataset Blur', type=str, help='dataset name')
+parser.add_argument('--exp', default='lowlight_blur', type=str, help='experiment setting')
+parser.add_argument('--gpu', default='0', type=str, help='GPUs used for training')
+parser.add_argument('--resume', type=bool, default=True, help='Continue Train')
+args = parser.parse_args()
+
+os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+
+
+def train(train_loader, network, criterion, optimizer, scaler):
+	losses = AverageMeter()
+
+	torch.cuda.empty_cache()
+	
+	network.train()
+
+	for batch in train_loader:
+		source_img = batch['source'].cuda()
+		target_img = batch['target'].cuda()
+
+		with autocast(args.no_autocast):
+			output, R_low, R_high, I_high_3, I_delta_3, lle_gt, I_re = network(source_img, target_img)
+			
+			main_loss = criterion[1](output, target_img)
+			high_loss = criterion[0](R_high*I_high_3, lle_gt)
+			mutal_high_loss = criterion[0](R_low*I_high_3, lle_gt)
+			mutal_low_loss = criterion[0](R_high*I_delta_3, lle_gt)
+			relight_loss = criterion[0](R_low*I_delta_3, lle_gt)
+			equal_R_loss = criterion[0](R_low, R_high)
+			input_loss = criterion[1](source_img, I_re)
+
+			cr_loss = criterion[2](target_img, output, source_img, I_re)
+
+			loss = main_loss + 0.5*input_loss + 0.2*(relight_loss + high_loss + 0.001*mutal_high_loss + 0.001*mutal_low_loss + 0.01*equal_R_loss) + 0.1*cr_loss
+
+		losses.update(loss.item())
+
+		optimizer.zero_grad()
+		scaler.scale(loss).backward()
+		scaler.step(optimizer)
+		scaler.update()
+
+	return losses.avg
+
+
+def valid(val_loader, network):
+	PSNR = AverageMeter()
+
+	torch.cuda.empty_cache()
+
+	network.eval()
+
+	for batch in val_loader:
+		source_img = batch['source'].cuda()
+		target_img = batch['target'].cuda()
+
+		with torch.no_grad():		
+			output, R_low, R_high, I_high_3, I_delta_3, lle_gt, I_re = network(source_img, target_img)
+			output = output.clamp_(-1, 1)		
+
+		mse_loss = F.mse_loss(output * 0.5 + 0.5, target_img * 0.5 + 0.5, reduction='none').mean((1, 2, 3))
+		psnr = 10 * torch.log10(1 / mse_loss).mean()
+		PSNR.update(psnr.item(), source_img.size(0))
+
+	return PSNR.avg
+
+
+if __name__ == '__main__':
+	
+	setting_filename = 'configs.json'
+	with open(setting_filename, 'r') as f:
+		setting = json.load(f)
+
+	network = eval(args.model.replace('-', '_'))()
+	network = nn.DataParallel(network).cuda()
+
+	criterion = []
+	criterion.append(nn.L1Loss().cuda())
+	criterion.append(nn.SmoothL1Loss().cuda())
+	criterion.append(ContrastLoss(ablation=False))
+
+	if setting['optimizer'] == 'adam':
+		optimizer = torch.optim.Adam(network.parameters(), lr=setting['lr'])
+	elif setting['optimizer'] == 'adamw':
+		optimizer = torch.optim.AdamW(network.parameters(), lr=setting['lr'])
+	else:
+		raise Exception("ERROR: unsupported optimizer") 
+
+	scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_mult=2, last_epoch=-1, 
+                                                                     T_0=setting['step_T'],
+                                                                     eta_min=setting['lr'] * 1e-3)
+
+	scaler = GradScaler()
+
+	dataset_dir = os.path.join(args.data_dir, args.dataset)
+	train_dataset = PairLoader(dataset_dir, 'train', 'train', 
+								setting['patch_size'], setting['edge_decay'], setting['only_h_flip'])
+	train_loader = DataLoader(train_dataset,
+                              batch_size=setting['batch_size'],
+                              shuffle=True,
+                              num_workers=args.num_workers,
+                              pin_memory=True,
+                              drop_last=True)
+	val_dataset = PairLoader(dataset_dir, 'test', setting['valid_mode'], 
+							  setting['patch_size'])
+	val_loader = DataLoader(val_dataset,
+                            batch_size=int(setting['batch_size']/1),
+                            num_workers=args.num_workers,
+                            pin_memory=True)
+
+	save_dir = os.path.join(args.save_dir, args.exp)
+	os.makedirs(save_dir, exist_ok=True)
+
+	losses = []
+	start_epoch = 0
+	best_psnr = 0
+	psnrs = []
+
+	if args.resume and os.path.exists(os.path.join(save_dir, args.model+'.pth')):
+		ckp = torch.load(os.path.join(save_dir, args.model+'.pth'))
+		network.load_state_dict(ckp['state_dict'])
+		start_epoch = ckp['epoch']
+		best_psnr = ckp['best_psnr']
+		psnrs = ckp['psnrs']
+		print(f'start_step: {start_epoch} continue to train --- from best psnr {best_psnr}')
+	else:
+		print('==> Start training, current model name: ' + args.model)
+
+
+	writer = SummaryWriter(log_dir=os.path.join(args.log_dir, args.exp, args.model))
+	
+	for epoch in tqdm(range(setting['epochs'] + 1)):
+			loss = train(train_loader, network, criterion, optimizer, scaler)
+
+			writer.add_scalar('train_loss', loss, epoch)
+
+			scheduler.step()
+
+			if epoch % setting['eval_freq'] == 0:
+				avg_psnr = valid(val_loader, network)
+				
+				writer.add_scalar('valid_psnr', avg_psnr, epoch)
+				print(avg_psnr)
+
+				if avg_psnr > best_psnr:
+					best_psnr = avg_psnr
+					torch.save({'epoch': epoch,
+                            'best_psnr': best_psnr,
+                            'psnrs': psnrs,
+                            'state_dict': network.state_dict()},
+                           os.path.join(save_dir, args.model + '_' + str(epoch)+'.pth'))
+					
+					print(f'\n Models saved at epoch: {epoch} | best_psnr: {best_psnr:.4f}')
+				
+				writer.add_scalar('best_psnr', best_psnr, epoch)
